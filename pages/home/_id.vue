b9fb78faf5ad79d297163841e153426f740ddac7
Askar Karasaev <askar.karasaev@gmail.com>
2021-08-05 15:54:52 +0900

    Adding a page under home folder

---
+++ pages/home/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ home.title }}
+  </div>
+</template>
+<script>
+import homes from "~/data/homes";
+export default {
+  created() {
+    const home = homes.find((home) => home.objectID == this.$route.params.id);
+    this.home = home;
+  },
+};
+</script>
