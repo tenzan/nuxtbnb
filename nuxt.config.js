80613adfe0daeae36ac6c9ee7273397dc1909ce6
Askar Karasaev <askar.karasaev@gmail.com>
2021-08-05 15:27:48 +0900

    Adding Meta Tags and Other Elements to the HTML Head TagCreating Homepage

---
+++ nuxt.config.js
@@ -0,0 +1,17 @@
+export default {
+  components: true,
+  head: {
+    titleTemplate: "Mastering Nuxt: %s",
+    htmlAttrs: {
+      lang: "en",
+    },
+    bodyAttrs: {
+      class: ["my-style"],
+    },
+    meta: [
+      {
+        charset: "utf-8",
+      },
+    ],
+  },
+};
--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,18 @@
 import homes from "~/data/homes";
 
 export default {
+  head() {
+    return {
+      title: "Homepage",
+      meta: [
+        {
+          name: "description",
+          content: "This is a homepage!",
+          hid: "description",
+        },
+      ],
+    };
+  },
   data() {
     return {
       homes: homes.slice(0, 3),
