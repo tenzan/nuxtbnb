10fe4f71e701eedbdd6fecaa178027e2075ece51
Askar Karasaev <askar.karasaev@gmail.com>
2021-08-05 15:09:59 +0900

    Creating Homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    hello world
+    <div
+      v-for="home in homes"
+      :key="home.objectID"
+      style="float:left;margin:10px"
+    >
+      <home-card :home="home" />
+    </div>
   </div>
 </template>
+<script>
+import homes from "~/data/homes";
+
+export default {
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
+</script>
